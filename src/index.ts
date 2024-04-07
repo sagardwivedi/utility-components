4beb758d6103fba918c9f46fb33fe0c527b64c22
sagardwivedi <sagar.dw20@gmail.com>
2024-04-07 20:46:15 +0530

    made changes in the export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import Each from './Each';
-import Show from './Show';
+import Each from "./Each";
+import Show, { ShowElse, ShowWhen } from "./Show";
 
-export { Show, Each };
+export { Show, Each, ShowElse, ShowWhen };
